72bea253a96265dbd4f3578e19b8399ac7ee0519
TechBK <quangbinh.nguyentrong@gmail.com>
2015-10-22 09:46:53 +0700

    #6:....

--- openstack_dashboard/dashboards/mydashboard/mypanel/views.py
+++ openstack_dashboard/dashboards/mydashboard/mypanel/views.py
@@ -1,9 +1,11 @@
-from horizon import views
+from horizon import tabs
+
+from openstack_dashboard.dashboards.mydashboard.mypanel \
+    import tabs as mydashboard_tabs
 
 
-class IndexView(views.APIView):
-	tab_group_class = mydashboard_tabs.MypanelTabs
-    # A very simple class-based view...
+class IndexView(tabs.TabbedTableView):
+    tab_group_class = mydashboard_tabs.MypanelTabs
     template_name = 'mydashboard/mypanel/index.html'
 
     def get_data(self, request, context, *args, **kwargs):
